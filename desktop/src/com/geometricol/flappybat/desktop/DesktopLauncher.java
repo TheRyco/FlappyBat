9f5c18038de56bc68f8a930fbb35bfda205ead82
TheRyco <sericose@aol.com>
2015-09-24 14:18:37 +0200

    Initial commit

---
+++ desktop/src/com/geometricol/flappybat/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.geometricol.flappybat.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.geometricol.flappybat.flappybatgame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new flappybatgame(), config);
+	}
+}
